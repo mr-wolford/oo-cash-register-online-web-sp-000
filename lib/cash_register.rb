958cedf919547d8a29e7afdb061c97fcf3c57efd
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-13 20:56:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  attr_accessor :discount, :total
 
+@items = []
+
+def initialize(discount = nil)
+  @total = 0
+  @discount = discount
+end
+
+def total
+  @total
+end
+
+def add_item(title, price, quantity = 1)
+  adjusted_price = price * quantity
+  @total = @total + adjusted_price
+  @items << title
+end
+
+
+
+end
